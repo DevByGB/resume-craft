4fdfe51fc3981a094b8f35af3a7df4be6a1bc9df
Gabriel Borges <contato@gbdev.me>
2024-08-17 15:09:15 -0300

    feat: criando multiple drag list

--- src/components/pages/dashboard/resume/index.tsx
+++ src/components/pages/dashboard/resume/index.tsx
@@ -1,30 +1,75 @@
+"use client"
+
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/components/ui/resizable"
-import { InfosSidebar } from "./infos-sidebar"
-import { ResumeContent } from "./resume-content"
-import { StructureSidebar } from "./structure-sidebar"
+} from "@/components/ui/resizable";
+import { InfosSidebar } from "./infos-sidebar";
+import { ResumeContent } from "./resume-content";
+import { StructureSidebar } from "./structure-sidebar";
+import { FormProvider, useForm } from "react-hook-form";
 
 export const ResumePage = () => {
+  const defaultValues: ResumeData = {
+    content: {
+      image: {
+        url: "",
+        visible: true,
+      },
+      infos: {
+        email: "",
+        fullName: "",
+        headline: "",
+        location: "",
+        phone: "",
+        website: "",
+      },
+      summary: "",
+      certifications: [],
+      educations: [],
+      experiences: [],
+      languages: [],
+      projects: [],
+      skills: [],
+      socialMedias: [
+        {
+          icon: "",
+          name: "test 1",
+          url: "test 1",
+          username: "test 1",
+        },
+        {
+          icon: "",
+          name: "test 2",
+          url: "test 2",
+          username: "test 2",
+        }
+      ],
+    },
+  };
+
+  const methods = useForm<ResumeData>({ defaultValues });
+
   return (
-    <main className="w-full h-screen overflow-hidden">
-      <ResizablePanelGroup direction="horizontal" className="w-full h-full">
-        <ResizablePanel minSize={20} maxSize={40} defaultSize={30}>
-          <InfosSidebar />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
+    <FormProvider {...methods}>
+      <main className="w-full h-screen overflow-hidden">
+        <ResizablePanelGroup direction="horizontal" className="w-full h-full">
+          <ResizablePanel minSize={20} maxSize={40} defaultSize={30}>
+            <InfosSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
 
-        <ResizablePanel>
-          <ResumeContent />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
+          <ResizablePanel>
+            <ResumeContent />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
 
-        <ResizablePanel minSize={20} maxSize={35} defaultSize={25}>
-          <StructureSidebar />
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </main>
-  )
-}
+          <ResizablePanel minSize={20} maxSize={35} defaultSize={25}>
+            <StructureSidebar />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </main>
+    </FormProvider>
+  );
+};
